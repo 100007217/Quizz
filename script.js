31f0bd57dc4e6f1be51f4bc3c30f7dbf24d65598
100007217 <gomezmonterroso14@gmail.com>
2022-02-23 15:34:50 +0100

    Siguientes preguntas + emojis

--- script.js
+++ script.js
@@ -15,44 +15,82 @@ var getJSON = function(url, callback) {
     };
     xhr.send();
 };
-getJSON('https://opentdb.com/api.php?amount=2&category=9&difficulty=easy&type=multiple',
-    function(err, data) {
-        if (err !== null) {
-            alert('Something went wrong: ' + err);
-        } else {
-            datos = data['results']
-            console.log(datos)
-            var pregunta = document.getElementById('pregunta')
-            pregunta.innerHTML = datos[0]['question']
-            var respuestas = document.getElementsByClassName('respuestas')
-            var posiciones_disponibles = [0, 1, 2, 3]
+var numero_pregunta = 0
 
-            function insertar_respuesta_correcta() {
-                random = Math.floor(Math.random() * 4);
-                respuestas[random].innerHTML = datos[0]['correct_answer']
-                posiciones_disponibles.splice(random, 1);
-                //console.log("He metido en la posicon " + random + " la repsuesta correcta que es " + datos[0]['correct_answer'])
-                //console.log("Quedan las repsuestas " + posiciones_disponibles)
-            }
-            insertar_respuesta_correcta();
-
-            function rellenar_respuestas_incorrectas(posiciones_disponibles) {
-                for (let i = 0; i < 3; i++) {
-                    posicion = posiciones_disponibles[i]
-                    respuestas[posicion].innerHTML = datos[0]['incorrect_answers'][i]
-                }
+function pillar_datos() {
+    getJSON('https://opentdb.com/api.php?amount=2&category=9&difficulty=easy&type=multiple',
+        function(err, data) {
+            if (err !== null) {
+                alert('Something went wrong: ' + err);
+            } else {
+                datos = data['results']
+                    //console.log(datos)
+                return datos
             }
         }
-        rellenar_respuestas_incorrectas(posiciones_disponibles)
-    }
-)
+    )
+}
+var xhReq = new XMLHttpRequest();
+xhReq.open("GET", 'https://opentdb.com/api.php?amount=2&category=9&difficulty=easy&type=multiple', false);
+xhReq.send(null);
+var jsonObject = JSON.parse(xhReq.responseText);
+datos = jsonObject['results']
+    //console.log(datos)
+var numero_pregunta = 0
+var pregunta = document.getElementById('pregunta')
+var respuestas = document.getElementsByClassName('respuestas')
 
-function seleccionarRespuesta(id) {
+
+
+
+function insertar_respuesta_correcta(numero_pregunta, datos) {
+    console.log(datos)
+    var posiciones_disponibles = [0, 1, 2, 3]
+    random = Math.floor(Math.random() * 4);
+    pregunta.innerHTML = datos[numero_pregunta]['question']
+    respuestas[random].innerHTML = datos[numero_pregunta]['correct_answer']
+        //console.log(respuestas[random])
+    posiciones_disponibles.splice(random, 1);
+    console.log("He metido en la posicon " + random + " la repsuesta correcta que es " + datos[numero_pregunta]['correct_answer'])
+        //console.log("Quedan las repsuestas " + posiciones_disponibles)
+    rellenar_respuestas_incorrectas(posiciones_disponibles, numero_pregunta)
+}
+
+function rellenar_respuestas_incorrectas(posiciones_disponibles, numero_pregunta) {
+    for (let i = 0; i < 3; i++) {
+        posicion = posiciones_disponibles[i]
+        respuestas[posicion].innerHTML = datos[numero_pregunta]['incorrect_answers'][i]
+    }
+}
+insertar_respuesta_correcta(numero_pregunta, datos)
+
+function seleccionarRespuesta(id, numero_pregunta) {
     respuesta_seleccionada = document.getElementById(id)
-    if (respuesta_seleccionada.innerHTML == datos[0]['correct_answer']) {
+    respuestas_correctas = document.getElementById('num_correctas')
+    if (respuesta_seleccionada.innerHTML == datos[numero_pregunta]['correct_answer']) {
         respuesta_seleccionada.style.backgroundColor = "lightgreen"
+        respuestas_correctas.innerHTML += '&#9989;'
+
     } else {
         respuesta_seleccionada.style.backgroundColor = "lightcoral"
-        setTimeout(() => { console.log("World!"); }, 2000);
+        respuestas_correctas.innerHTML += '&#10060;'
     }
+    var respuestas = document.getElementsByClassName('respuestas')
+        //document.getElementById('cmdt_1_1i').removeAttribute("onclick");
+    for (let i = 0; i < 4; i++) {
+        respuestas[i].removeAttribute("onclick")
+    }
+}
+
+function siguientePregunta(numero_pregunta) {
+    for (let i = 0; i < 4; i++) {
+        //elem.setAttribute("onclick", "alert('blah');");
+        //respuestas[i].setAttribute('onclick', 'seleccionarRespuesta(respuesta' + (i + 1) + ',' + numero_pregunta + ')')
+        respuestas[i].onclick = function() { seleccionarRespuesta(this.id, numero_pregunta) };
+
+        respuestas[i].style.backgroundColor = "lightgray"
+
+    }
+    numero_pregunta += 1
+    insertar_respuesta_correcta(numero_pregunta, datos)
 }
